304703a291dadb21b1e8bb9083077f94bcd76481
Ana Castilla <ana.castilla1998@gmail.com>
2020-10-21 15:56:11 +0200

    Commit

--- app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
@@ -1,11 +1,10 @@
 package es.iessaladillo.pedrojoya.exchange
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.ImageView
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.widget.*
 import androidx.annotation.DrawableRes
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
@@ -26,7 +25,81 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
-
+        binding.rdbFromDollar.isEnabled = false
+        binding.rdbToEuro.isEnabled = false
+        binding.btnExchange.setOnClickListener { btnExchangeOnClick() }
+        binding.rdgFrom.setOnCheckedChangeListener { currency, id -> checkedCurrencies(currency, id) }
+        binding.rdgTo.setOnCheckedChangeListener { currency, id -> checkedCurrencies(currency, id) }
     }
 
+    private fun changeIconCurrency(imgIcon: ImageView, currency: Currency) {
+        imgIcon.setImageResource(currency.drawableResId)
+    }
+
+    private fun enableCurrency(dollar: Boolean, euro: Boolean, pound: Boolean, currency: RadioGroup) {
+        if (currency != binding.rdgFrom) {
+            binding.rdbFromDollar.isEnabled = dollar
+            binding.rdbFromEuro.isEnabled = euro
+            binding.rdbFromPound.isEnabled = pound
+        } else {
+            binding.rdbToDollar.isEnabled = dollar
+            binding.rdbToEuro.isEnabled = euro
+            binding.rdbToPound.isEnabled = pound
+        }
+    }
+
+    private fun checkedCurrencies(checkedCurrency: RadioGroup, checkedId: Int) {
+        when(checkedId) {
+            binding.rdbFromDollar.id -> {
+                changeIconCurrency(binding.imgFrom, Currency.DOLLAR)
+                enableCurrency(false, true, true, currency = checkedCurrency)
+            }
+            binding.rdbFromEuro.id -> {
+                changeIconCurrency(binding.imgFrom, Currency.EURO)
+                enableCurrency(true, false, true, currency = checkedCurrency)
+            }
+            binding.rdbFromPound.id -> {
+                changeIconCurrency(binding.imgFrom, Currency.POUND)
+                enableCurrency(true, true, false, currency = checkedCurrency)
+            }
+            binding.rdbToDollar.id -> {
+                changeIconCurrency(binding.imgTo, Currency.DOLLAR)
+                enableCurrency(false, true, true, currency = checkedCurrency)
+            }
+            binding.rdbToEuro.id -> {
+                changeIconCurrency(binding.imgTo, Currency.EURO)
+                enableCurrency(true, false, true, currency = checkedCurrency)
+            }
+            binding.rdbToPound.id -> {
+                changeIconCurrency(binding.imgTo, Currency.POUND)
+                enableCurrency(true, true, false, currency = checkedCurrency)
+            }
+        }
+    }
+
+    private fun validateAmount(amount: Double) : Boolean {
+       return if (!amount.equals(0)) {
+           true
+        } else {
+           binding.edtAmount.setText(R.string.amount)
+           false
+       }
+    }
+
+    private fun btnExchangeOnClick() {
+        val amount = binding.edtAmount.text.toString().toDouble()
+        if (validateAmount(amount)) {
+            SoftInputUtils.hideSoftKeyboard(binding.edtAmount)
+
+        }
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun exchange(currency: Double, result: Double, currencyFrom: Currency, currencyTo: Currency ) {
+        Toast.makeText(this, getString(R.string.result, currency, currencyFrom, result, currencyTo), Toast.LENGTH_SHORT).show();
+    }
+
+
+
 }
+
--- app/src/main/java/es/iessaladillo/pedrojoya/exchange/SoftInputUtils.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/exchange/SoftInputUtils.kt
@@ -1,4 +1,15 @@
 package es.iessaladillo.pedrojoya.exchange
 
-class SoftInputUtils {
-}
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+    object SoftInputUtils {
+
+        fun hideSoftKeyboard(view: View): Boolean {
+            val imm = view.context.getSystemService(
+                Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            return imm?.hideSoftInputFromWindow(view.windowToken, 0) ?: false
+        }
+
+    }
